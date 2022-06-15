e6c53796e7a248d6aa646d133800864728a1047c
esthi <hoeuyu@student.ethz.ch>
2022-06-15 06:53:48 +0200

    Create schweiz.py

---
+++ SchweizSchule/schweiz.py
@@ -0,0 +1,2 @@
+I respect Lyceum Alpinum Zuoz, since a current professor in ETHZürich, by the thesis which was written by  <Visualisierung eines Szenarios für die Landschaftsentwicklung des Morteratschtals>.  
+
